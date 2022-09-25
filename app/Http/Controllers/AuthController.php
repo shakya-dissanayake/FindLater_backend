446a621dfbf28107c06ea151ff85dcdd4850d5a6
[Shakya Dissanayake] <[info.dev.shakya@gmail.com]>
2022-09-25 20:18:33 +0530

    create Auth Controller
    - Create Auth Controller
    - Add testing routes
    - Made RegisterUserRequest
    (didn't add rules)

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,4 +8,16 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     use HttpResponses;
+
+    public function login(){
+        return 'Hello Login!';
+    }
+
+    public function register(){
+        return response()->json('Hello Register!');
+    }
+
+    public function logout(){
+        return response()->json('Hello Logout!');
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('logout', [AuthController::class, 'logout']);
+
+Route::resource('places', PlaceController::class);
